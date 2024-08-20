Fix nan propagation in backprop

stable_baselines3/common/buffers.py
@@ -258,7 +258,7 @@ class ReplayBuffer(BaseBuffer):
         reward: np.ndarray,
         done: np.ndarray,
         infos: List[Dict[str, Any]] = None,
-        pseudo_rewards = None
+        pseudo_reward = None
     ) -> None:
                 
         # Reshape needed when using multiple envs with discrete observations
@@ -282,7 +282,10 @@ class ReplayBuffer(BaseBuffer):
         self.rewards[self.pos] = np.array(reward)
         self.dones[self.pos] = np.array(done)
         # Only happens for ssl buffer
-        if self.pseudo_mode: self.pseudo_rewards[self.pos] = np.array(pseudo_rewards)
+        if self.pseudo_mode: 
+            self.pseudo_rewards[self.pos] = np.array(pseudo_reward)
+            print("pseudo: ", pseudo_reward)
+
 
 
         # Issue here. We use extend and add to copy data from unlabeled buffer to ssl buffer, but SB3 not made for this. We don't have 'infos' as a dictionary when copying data since these Replay buffers only keep the timeouts. 
@@ -328,7 +331,6 @@ class ReplayBuffer(BaseBuffer):
             next_obs = self._normalize_obs(self.observations[(batch_inds + 1) % self.buffer_size, env_indices, :], env)
         else:
             next_obs = self._normalize_obs(self.next_observations[batch_inds, env_indices, :], env)
-
         
         data = (
             self._normalize_obs(self.observations[batch_inds, env_indices, :], env),
@@ -340,7 +342,9 @@ class ReplayBuffer(BaseBuffer):
             self._normalize_reward(self.rewards[batch_inds, env_indices].reshape(-1, 1), env),
         )
         
-        if self.pseudo_mode: data = data + (self.pseudo_rewards[batch_inds, env_indices], )
+
+        if self.pseudo_mode: 
+            data = data + (self.pseudo_rewards[batch_inds, env_indices], )
 
         return ReplayBufferSamples(*tuple(map(self.to_torch, data)))
 

stable_baselines3/common/off_policy_algorithm.py
@@ -364,7 +364,6 @@ class OffPolicyAlgorithm(BaseAlgorithm):
         )
 
         callback.on_training_start(locals(), globals())
-        print("Learning starts")
         
         assert self.env is not None, "You must set the environment before calling learn()"
         assert isinstance(self.train_freq, TrainFreq)  # check done in _setup_learn()
@@ -390,7 +389,7 @@ class OffPolicyAlgorithm(BaseAlgorithm):
             if not rollout.continue_training:
                 break
             
-            if  self.pseudo_mode and self.num_timesteps % self.ssl_freq == 0 and self.num_timesteps > self.learning_starts // 2:
+            if self.pseudo_mode and self.num_timesteps % self.ssl_freq == 0 and self.num_timesteps > self.learning_starts // 2:
                 # Completely reset the ssl_replay_buffer
                 # print(self.num_timesteps)
                 self.ssl_steps += 1
@@ -419,8 +418,6 @@ class OffPolicyAlgorithm(BaseAlgorithm):
 
                 rewards = labeled_replay_data.rewards.numpy()
 
-                print("Rewards", rewards.shape)
-
                 W = construct_connected_W(X_prime)
 
                 train_labels, unique_rewards, num_unique_labels, l2r = rewards_to_labels(rewards = rewards.astype(int))
@@ -443,13 +440,8 @@ class OffPolicyAlgorithm(BaseAlgorithm):
 
                 pseudo_rewards = th.tensor(list(map(lambda l: l2r[l.item()], pred_labels)))
 
-                print("Train labels", train_labels)
-                print("Predicted rewards", pseudo_rewards[:20])
-                print("True rewards", true_rewards[:20])
 
                 f1 = f1_score(np.delete(pseudo_rewards, train_ind), np.delete(true_rewards,train_ind), average='micro')
-
-                print(f1)
 
                 self.logger.record("ssl/steps", self.ssl_steps)
                 self.logger.record("ssl/f1_score", f1)
@@ -463,8 +455,10 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                     unlabeled_replay_data.rewards,
                     unlabeled_replay_data.dones,
                     self.unlabeled_replay_buffer.timeouts[np.arange(self.unlabeled_replay_buffer.size())],
-                    pseudo_rewards = np.delete(pseudo_rewards, train_ind)
+                    np.delete(pseudo_rewards, train_ind)
                 )
+
+                print("Size", self.ssl_replay_buffer.size())
 
 
             # Train using all labeled and unlabeled data. 